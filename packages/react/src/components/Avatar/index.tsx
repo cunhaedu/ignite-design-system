1440a6a399e38ec5d44ecfdda412752f32bf5ac3
Eduardo Assunção <cunhaeduardo1231@gmail.com>
2022-10-08 20:50:22 -0300

    feat: add Avatar component

---
+++ packages/react/src/components/Avatar/index.tsx
@@ -0,0 +1,17 @@
+import { AvatarContainer, AvatarFallback, AvatarImage } from './styles'
+import { User } from 'phosphor-react'
+import { ComponentProps } from 'react'
+
+export type AvatarProps = ComponentProps<typeof AvatarImage>
+
+export function Avatar(props: AvatarProps) {
+  return (
+    <AvatarContainer>
+      <AvatarImage {...props} />
+
+      <AvatarFallback delayMs={600}>
+        <User />
+      </AvatarFallback>
+    </AvatarContainer>
+  )
+}
