018a72eaaf15c873369bb494afe7d65103856347
Eduardo Assunção <cunhaeduardo1231@gmail.com>
2022-10-08 14:04:21 -0300

    feat(react): add stitches

---
+++ packages/react/src/index.tsx
@@ -0,0 +1,11 @@
+// import { styled } from './styles'
+
+// const Button = styled('button', {
+//   fontFamily: '$default',
+//   backgroundColor: '$ignite500',
+//   borderRadius: '$md',
+// })
+
+export function App() {
+  return <h1>Hello World</h1>
+}
