573b10dd68257685d2e1f2488b77b4b82d6b8b95
Eduardo Assunção <cunhaeduardo1231@gmail.com>
2022-10-09 14:55:19 -0300

    feat: add multistep component

--- packages/react/src/index.tsx
+++ packages/react/src/index.tsx
@@ -1,8 +1,16 @@
+// SURFACES
 export * from './components/Card'
-export * from './components/Text'
+
+// TYPOGRAPHY
 export * from './components/Heading'
-export * from './components/Button'
-export * from './components/TextArea'
-export * from './components/Checkbox/index'
+export * from './components/Text'
+
+// DATA DISPLAY
 export * from './components/Avatar/index'
+
+// FORMS
 export * from './components/TextInput/index'
+export * from './components/MultiStep/index'
+export * from './components/Checkbox/index'
+export * from './components/TextArea'
+export * from './components/Button'
